dbc4fd197e6a841384eff6fd1104045623dc6d82
Bulkan Evcimen <bulkan@gmail.com>
2016-06-03 14:25:14 +1000

    second pass

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,10 +1,15 @@
 (ns rna-transcription)
 
-(def rna {:G "C"
-          :C "G"
-          :T "A"
-          :A "U"})
+(def rna {"G" "C"
+          "C" "G"
+          "T" "A"
+          "A" "U"})
+
+(defn dna-to-rna
+  [nucleotide]
+  (assert (not contains? rna nucleotide))
+  (get rna nucleotide))
 
 (defn to-rna
   [dna]
-  (str dna))
+  (map dna-to-rna dna))

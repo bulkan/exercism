ead0f94e72b56b1832a27cc34cf736acd1295d6c
Bulkan Evcimen <bulkan@gmail.com>
2016-06-07 16:59:09 +1000

    [anagram] second pass

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,8 +1,23 @@
 (ns anagram)
 
-(defn anagrams-for [word candidates]
-  (println word))
+(defn check-length [a b]
+  (= (count a) (count b)))
 
-; check length of candidate = word
+(defn words [word candidates]
+  (filter
+    (partial check-length word)
+    candidates))
+
+
+
+(defn anagrams-for [word candidates]
+  (words word candidates))
+
 ; reduce candidates
-; check letters 
+; check letters
+; create a set from the words and they should be equal
+
+
+; An anagram is direct word switch or word play, the result of rearranging the
+; letters of a word or phrase to produce a new word or phrase, using all the
+; original letters exactly once

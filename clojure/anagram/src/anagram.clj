57957ec7fc2d48d4bca69e95b6a0c93f99f28d78
Bulkan Evcimen <bulkan@gmail.com>
2016-06-09 12:22:49 +1000

    anagram solution

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,23 +1,14 @@
-(ns anagram)
-
-(defn check-length [a b]
-  (= (count a) (count b)))
-
-(defn words [word candidates]
-  (filter
-    (partial check-length word)
-    candidates))
-
-
+(ns anagram
+  (:require [clojure.string :refer [lower-case]]))
 
 (defn anagrams-for [word candidates]
-  (words word candidates))
-
-; reduce candidates
-; check letters
-; create a set from the words and they should be equal
-
-
-; An anagram is direct word switch or word play, the result of rearranging the
-; letters of a word or phrase to produce a new word or phrase, using all the
-; original letters exactly once
+  (reduce
+    (fn [final candidate]
+      (let [word (lower-case word) lower-cased-candidate (lower-case candidate)]
+        (if (and
+              (not (= word lower-cased-candidate))
+              (= (frequencies word) (frequencies lower-cased-candidate)))
+            (conj final candidate)
+            final)))
+    []
+    candidates))
